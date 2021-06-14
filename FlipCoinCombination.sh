0c4485f92532ef1e8f2dd8559d10bb5f094849b5
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-14 11:41:47 +0530

    adding the singlet combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,8 +1,26 @@
+#!/bin/bash -x
 echo "Flip coin combinaion"
-random=$((RANDOM%2))
-if [ $random -eq 0 ]
-then
-	echo "heads win"
-else
-	echo "Tails win"
-fi
+HeadCount=0
+TailCount=0
+declare -A arr
+for i in {1..10}
+do
+	flip1=$((RANDOM%2))
+	if [ $flip1 -eq 0 ]
+	then
+		echo "flip1 heads"
+		arr[$i]="Heads"
+                HeadCount=$(($HeadCount+1))
+	else
+		echo "flip1 tails"
+		arr[$i]="tails"
+                TailCount=$(($TailCount+1))
+	fi
+done
+
+HeadsPercent=`echo $HeadCount ${#arr[@]} 100 | awk '{print $1 / $2 * $3 }'`
+echo $HeadsPercent
+TailsPercent=`echo $TailCount ${#arr[@]} 100 | awk '{print $1 / $2 * $3 }'`
+echo $TailsPercent
+
+echo ${arr[@]}
