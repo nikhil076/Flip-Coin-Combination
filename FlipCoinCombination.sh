676f143b63a1d6a43043e748f258612489cf626c
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-14 12:02:46 +0530

    adding the doublet combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,26 +1,44 @@
 #!/bin/bash -x
 echo "Flip coin combinaion"
-HeadCount=0
-TailCount=0
+BothHeadCount=0
+BothTailCount=0
+HeadTailCount=0
+TailHeadCount=0
 declare -A arr
-for i in {1..10}
+for i in {1..20}
 do
 	flip1=$((RANDOM%2))
-	if [ $flip1 -eq 0 ]
+	flip2=$((RANDOM%2))
+	if [ $flip1 -eq 0 ] && [ $flip2 -eq 0 ]
 	then
-		echo "flip1 heads"
-		arr[$i]="Heads"
-                HeadCount=$(($HeadCount+1))
-	else
-		echo "flip1 tails"
-		arr[$i]="tails"
-                TailCount=$(($TailCount+1))
+		echo "Both Heads"
+		arr[$i]="HH"
+                BothHeadCount=$(($BothHeadCount+1))
+	elif [ $flip1 -eq 1 ] && [ $flip2 -eq 1 ]
+	then
+		echo "Both Tails"
+		arr[$i]="TT"
+                BothTailCount=$(($BothTailCount+1))
+	elif [ $flip1 -eq 0 ] && [ $flip2 -eq 1 ]
+	then
+		echo "Head aand tail"
+		arr[$i]="HT"
+		HeadTailCount=$(($HeadTailCount+1))
+	elif [ $flip1 -eq 1 ] && [ $flip2 -eq 0 ]
+	then
+		echo "Tail and Head"
+		arr[$i]="TH"
+		TailHeadCount=$(($TailHeadCount+1))
 	fi
 done
 
-HeadsPercent=`echo $HeadCount ${#arr[@]} 100 | awk '{print $1 / $2 * $3 }'`
-echo $HeadsPercent
-TailsPercent=`echo $TailCount ${#arr[@]} 100 | awk '{print $1 / $2 * $3 }'`
-echo $TailsPercent
+BothHeadsPercent=`echo $BothHeadCount ${#arr[@]} 100 | awk '{print $1 / $2 * $3 }'`
+echo $BothHeadsPercent
+BothTailsPercent=`echo $BothTailCount ${#arr[@]} 100 | awk '{print $1 / $2 * $3 }'`
+echo $BothTailsPercent
+HeadTailPercent=`echo $HeadTailCount ${#arr[@]} 100 | awk '{print $1 / $2 * $3 }'`
+echo $HeadTailPercent
+TailHeadPercent=`echo $TailHeadCount ${#arr[@]} 100 | awk '{print $1 / $2 * $3 }'`
+echo $TailHeadPercent
 
 echo ${arr[@]}
